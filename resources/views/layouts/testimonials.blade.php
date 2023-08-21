348089bf2b7093afd7847c6f67b1d4024440536a
Endurance, the Martian <afriviral@gmail.com>
2023-08-21 19:48:50 +0100

    changes/ about static --done

---
+++ resources/views/layouts/testimonials.blade.php
@@ -0,0 +1,213 @@
+<div class="testimonial-section pt-80 pb-80">
+    <img src="assets/images/bg/client-right.png" class="client-right-vector" alt="">
+    <img src="assets/images/bg/client-left.png" class="client-left-vector" alt="">
+    <img src="assets/images/bg/clent-circle1.png" class="client-circle1" alt="">
+    <img src="assets/images/bg/clent-circle2.png" class="client-circle2" alt="">
+    <img src="assets/images/bg/clent-circle3.png" class="client-circle3" alt="">
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-sm-12 col-md-10 col-lg-8 col-xl-6">
+                <div class="section-title1">
+                    <h2>What Client Say</h2>
+                    <p class="mb-0">Explore on the world's best &amp; largest Bidding marketplace with our beautiful
+                        Bidding
+                        products. We want to be a part of your smile, success and future growth.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center position-relative">
+            <div class="swiper testimonial-slider2 swiper-initialized swiper-horizontal swiper-android">
+                <div class="swiper-wrapper" id="swiper-wrapper-8be698db210fb5d1a" aria-live="polite"
+                    style="transform: translate3d(-1314px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active"
+                        data-swiper-slide-index="0" style="width: 414px; margin-right: 24px;" role="group"
+                        aria-label="1 / 3">
+                        <div class="testimonial-single style2 hover-border1 wow fadeInDown" data-wow-duration="1.5s"
+                            data-wow-delay="0.3s"
+                            style="visibility: visible; animation-duration: 1.5s; animation-delay: 0.3s; animation-name: fadeInDown;">
+                            <img src="assets/images/icons/quote.svg" class="quote-icon" alt="quote-icon">
+                            <div class="testi-img">
+                                <img src="assets/images/bg/testi1.png" alt="">
+                            </div>
+                            <div class="testi-content">
+                                <div class="testi-designation">
+                                    <h5>Johan Martin R</h5>
+                                    <p>CEO Founder</p>
+                                </div>
+                                <p class="para">The Pacific Grove Chamber of Commerce would like to thank eLab
+                                    Communications and Mr. Will Elkadi for all the efforts that
+                                    assisted me nicely manners. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-next"
+                        data-swiper-slide-index="1" style="width: 414px; margin-right: 24px;" role="group"
+                        aria-label="2 / 3">
+                        <div class="testimonial-single style2 hover-border1 wow fadeInDown" data-wow-duration="1.5s"
+                            data-wow-delay="0.6s"
+                            style="visibility: visible; animation-duration: 1.5s; animation-delay: 0.6s; animation-name: fadeInDown;">
+                            <img src="assets/images/icons/quote.svg" class="quote-icon" alt="quote-icon">
+                            <div class="testi-img">
+                                <img src="assets/images/bg/testi2.png" alt="">
+                            </div>
+                            <div class="testi-content">
+                                <div class="testi-designation">
+                                    <h5>Jamie Anderson</h5>
+                                    <p>Founder</p>
+                                </div>
+                                <p class="para">Maecenas vitae porttitor neque, ac porttitor nunc. Duis venenatis
+                                    lacinia libero. Nam nec augue ut nunc vulputate tincidunt at suscipit nunc. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide swiper-slide-duplicate swiper-slide-prev" data-swiper-slide-index="2"
+                        style="width: 414px; margin-right: 24px;" role="group" aria-label="3 / 3">
+                        <div class="testimonial-single style2 hover-border1 wow fadeInDown" data-wow-duration="1.5s"
+                            data-wow-delay="0.9s"
+                            style="visibility: visible; animation-duration: 1.5s; animation-delay: 0.9s; animation-name: fadeInDown;">
+                            <img src="assets/images/icons/quote.svg" class="quote-icon" alt="quote-icon">
+                            <div class="testi-img">
+                                <img src="assets/images/bg/testi3.png" alt="">
+                            </div>
+                            <div class="testi-content">
+                                <div class="testi-designation">
+                                    <h5>John peter</h5>
+                                    <p>CEO Founder</p>
+                                </div>
+                                <p class="para">Nullam cursus tempor ex. Nullam nec dui id metus consequat congue ac at
+                                    est. Pellentesque blandit neque at elit tristique tincidunt.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide swiper-slide-active" data-swiper-slide-index="0"
+                        style="width: 414px; margin-right: 24px;" role="group" aria-label="1 / 3">
+                        <div class="testimonial-single style2 hover-border1 wow fadeInDown" data-wow-duration="1.5s"
+                            data-wow-delay="0.3s"
+                            style="visibility: visible; animation-duration: 1.5s; animation-delay: 0.3s; animation-name: fadeInDown;">
+                            <img src="assets/images/icons/quote.svg" class="quote-icon" alt="quote-icon">
+                            <div class="testi-img">
+                                <img src="assets/images/bg/testi1.png" alt="">
+                            </div>
+                            <div class="testi-content">
+                                <div class="testi-designation">
+                                    <h5>Johan Martin R</h5>
+                                    <p>CEO Founder</p>
+                                </div>
+                                <p class="para">The Pacific Grove Chamber of Commerce would like to thank eLab
+                                    Communications and Mr. Will Elkadi for all the efforts that
+                                    assisted me nicely manners. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide swiper-slide-next" data-swiper-slide-index="1"
+                        style="width: 414px; margin-right: 24px;" role="group" aria-label="2 / 3">
+                        <div class="testimonial-single style2 hover-border1 wow fadeInDown" data-wow-duration="1.5s"
+                            data-wow-delay="0.6s"
+                            style="visibility: visible; animation-duration: 1.5s; animation-delay: 0.6s; animation-name: fadeInDown;">
+                            <img src="assets/images/icons/quote.svg" class="quote-icon" alt="quote-icon">
+                            <div class="testi-img">
+                                <img src="assets/images/bg/testi2.png" alt="">
+                            </div>
+                            <div class="testi-content">
+                                <div class="testi-designation">
+                                    <h5>Jamie Anderson</h5>
+                                    <p>Founder</p>
+                                </div>
+                                <p class="para">Maecenas vitae porttitor neque, ac porttitor nunc. Duis venenatis
+                                    lacinia libero. Nam nec augue ut nunc vulputate tincidunt at suscipit nunc. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide swiper-slide-duplicate-prev" data-swiper-slide-index="2"
+                        style="width: 414px; margin-right: 24px;" role="group" aria-label="3 / 3">
+                        <div class="testimonial-single style2 hover-border1 wow fadeInDown" data-wow-duration="1.5s"
+                            data-wow-delay="0.9s"
+                            style="visibility: visible; animation-duration: 1.5s; animation-delay: 0.9s; animation-name: fadeInDown;">
+                            <img src="assets/images/icons/quote.svg" class="quote-icon" alt="quote-icon">
+                            <div class="testi-img">
+                                <img src="assets/images/bg/testi3.png" alt="">
+                            </div>
+                            <div class="testi-content">
+                                <div class="testi-designation">
+                                    <h5>John peter</h5>
+                                    <p>CEO Founder</p>
+                                </div>
+                                <p class="para">Nullam cursus tempor ex. Nullam nec dui id metus consequat congue ac at
+                                    est. Pellentesque blandit neque at elit tristique tincidunt.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active"
+                        data-swiper-slide-index="0" style="width: 414px; margin-right: 24px;" role="group"
+                        aria-label="1 / 3">
+                        <div class="testimonial-single style2 hover-border1 wow fadeInDown" data-wow-duration="1.5s"
+                            data-wow-delay="0.3s"
+                            style="visibility: visible; animation-duration: 1.5s; animation-delay: 0.3s; animation-name: fadeInDown;">
+                            <img src="assets/images/icons/quote.svg" class="quote-icon" alt="quote-icon">
+                            <div class="testi-img">
+                                <img src="assets/images/bg/testi1.png" alt="">
+                            </div>
+                            <div class="testi-content">
+                                <div class="testi-designation">
+                                    <h5>Johan Martin R</h5>
+                                    <p>CEO Founder</p>
+                                </div>
+                                <p class="para">The Pacific Grove Chamber of Commerce would like to thank eLab
+                                    Communications and Mr. Will Elkadi for all the efforts that
+                                    assisted me nicely manners. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-next"
+                        data-swiper-slide-index="1" style="width: 414px; margin-right: 24px;" role="group"
+                        aria-label="2 / 3">
+                        <div class="testimonial-single style2 hover-border1 wow fadeInDown" data-wow-duration="1.5s"
+                            data-wow-delay="0.6s"
+                            style="visibility: visible; animation-duration: 1.5s; animation-delay: 0.6s; animation-name: fadeInDown;">
+                            <img src="assets/images/icons/quote.svg" class="quote-icon" alt="quote-icon">
+                            <div class="testi-img">
+                                <img src="assets/images/bg/testi2.png" alt="">
+                            </div>
+                            <div class="testi-content">
+                                <div class="testi-designation">
+                                    <h5>Jamie Anderson</h5>
+                                    <p>Founder</p>
+                                </div>
+                                <p class="para">Maecenas vitae porttitor neque, ac porttitor nunc. Duis venenatis
+                                    lacinia libero. Nam nec augue ut nunc vulputate tincidunt at suscipit nunc. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="2"
+                        style="width: 414px; margin-right: 24px;" role="group" aria-label="3 / 3">
+                        <div class="testimonial-single style2 hover-border1 wow fadeInDown" data-wow-duration="1.5s"
+                            data-wow-delay="0.9s"
+                            style="visibility: visible; animation-duration: 1.5s; animation-delay: 0.9s; animation-name: fadeInDown;">
+                            <img src="assets/images/icons/quote.svg" class="quote-icon" alt="quote-icon">
+                            <div class="testi-img">
+                                <img src="assets/images/bg/testi3.png" alt="">
+                            </div>
+                            <div class="testi-content">
+                                <div class="testi-designation">
+                                    <h5>John peter</h5>
+                                    <p>CEO Founder</p>
+                                </div>
+                                <p class="para">Nullam cursus tempor ex. Nullam nec dui id metus consequat congue ac at
+                                    est. Pellentesque blandit neque at elit tristique tincidunt.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+            </div>
+            <div class="slider-arrows testimonial2-arrow d-flex justify-content-between gap-3">
+                <div class="testi-prev2 swiper-prev-arrow" tabindex="0" role="button" aria-label="Next slide"
+                    aria-controls="swiper-wrapper-8be698db210fb5d1a"><i class="bi bi-arrow-left"></i></div>
+                <div class="testi-next2 swiper-next-arrow" tabindex="0" role="button" aria-label="Previous slide"
+                    aria-controls="swiper-wrapper-8be698db210fb5d1a">
+                    <i class="bi bi-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,66 @@
+@extends('partials.app')
+@section('title', 'About')
+@section('description', 'Learn more about how we work and what we do.')
+@section('content')
+
+<div class="inner-banner">
+    <div class="container">
+        <h2 class="inner-banner-title wow fadeInLeft" data-wow-duration="1.5s" data-wow-delay=".2s"
+            style="visibility: visible; animation-duration: 1.5s; animation-delay: 0.2s; animation-name: fadeInLeft;">
+            About Us</h2>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">About Us</li>
+            </ol>
+        </nav>
+    </div>
+</div>
+
+<div class="about-section pt-120 pb-120">
+    <img src="assets/images/bg/section-bg.png" class="img-fluid section-bg-top" alt="section-bg">
+    <div class="container">
+        <div class="row d-flex justify-content-center g-4">
+            <div class="col-lg-6 col-md-10">
+                <div class="about-img-area">
+                    <div class="total-tag">
+                        <img src="assets/images/bg/total-tag.png" alt="">
+                        <h6>Total Raised</h6>
+                        <h5>$45,390.00</h5>
+                    </div>
+                    <img src="assets/images/bg/about-img.png" class="img-fluid about-img wow fadeInUp"
+                        data-wow-duration="1.5s" data-wow-delay=".2s" alt="about-img"
+                        style="visibility: visible; animation-duration: 1.5s; animation-delay: 0.2s; animation-name: fadeInUp;">
+                    <img src="assets/images/bg/about-linear.png" class="img-fluid about-linear" alt="">
+                    <img src="assets/images/bg/about-vector.png" class="img-fluid about-vector" alt="">
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-10">
+                <div class="about-content wow fadeInDown" data-wow-duration="1.5s" data-wow-delay=".2s"
+                    style="visibility: visible; animation-duration: 1.5s; animation-delay: 0.2s; animation-name: fadeInDown;">
+                    <span>Who we are!</span>
+                    <h2>We Work for Your Incredible Success</h2>
+                    <p class="para">Auction sites present consumers with a thrilling, competitive way to buy the goods
+                        and services they need most.</p>
+                    <p class="para">But getting your own auction site up and running has always required learning
+                        complex coding languages, or hiring an expensive design firm for thousands of dollars and months
+                        of work.</p>
+                    <ul class="about-list">
+                        <li><a href="#">Have enough food for life.</a></li>
+                        <li><a href="#">Poor children can return to school.</a></li>
+                        <li><a href="#">Fuga magni veritatis ad temporibus atque adipisci nisi rerum...</a></li>
+                    </ul>
+                    <a href="#choose-us" class="eg-btn btn--primary btn--md">More About</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@include('layouts.why-choose-us')
+
+@include('layouts.testimonials')
+
+@include('layouts.metrics')
+
+@endsection
