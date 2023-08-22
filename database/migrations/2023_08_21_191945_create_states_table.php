062dd3719f6fd26833557eb978dee1fd5bdfa66b
Endurance, the Martian <afriviral@gmail.com>
2023-08-22 18:52:25 +0100

    setup/ countries schema & migration

---
+++ database/migrations/2023_08_21_191945_create_states_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('country_id')->constrained('countries')->onDelete('cascade');
+            $table->string('name', 191)->index();
+            $table->string('code', 191)->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('states');
+    }
+};
