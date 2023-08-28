234a555d634c87a885f5c9155b44ba4f6d176ff9
Endurance, the Martian <afriviral@gmail.com>
2023-08-28 19:26:51 +0100

    added/ ckeditor textarea component

--- resources/views/components/textarea-field.blade.php
+++ resources/views/components/textarea-field.blade.php
@@ -3,3 +3,28 @@
     <textarea name="{{ $name }}" id="{{ $name }}" placeholder="{{ $placeholder }}" @class(['error'=> $errors->has($name)])></textarea>
     <span class="text-danger fs-6">{{ $errors->first($name) }}</span>
 </div>
+{{-- include ckeditor --}}
+@section('scripts')
+<script src="assets/js/ckeditor.js"></script>
+<script>
+    ClassicEditor
+        .create( document.querySelector( '#{{ $name }}' ), {
+            toolbar: {
+                items: [
+                    'heading', '|', 'bold', 'italic', 'bulletedList', 'numberedList', 'undo', 'redo'
+                ]
+            },
+            heading: {
+                options: [
+                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' }
+                ]
+            }
+        } )
+        .then( editor => {
+            console.log( editor );
+        } )
+        .catch( error => {
+            console.error( error );
+        } );
+</script>
+@endsection
