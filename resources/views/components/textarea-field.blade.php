29395b756d56391ffedff7a2ef80a84ada2796e4
Endurance, the Martian <afriviral@gmail.com>
2023-08-29 13:40:06 +0100

    added/ create new field function

--- resources/views/components/textarea-field.blade.php
+++ resources/views/components/textarea-field.blade.php
@@ -4,7 +4,7 @@
     <span class="text-danger fs-6">{{ $errors->first($name) }}</span>
 </div>
 {{-- include ckeditor --}}
-@section('scripts')
+@push('scripts')
 <script src="assets/js/ckeditor.js"></script>
 <script>
     ClassicEditor
@@ -16,7 +16,14 @@
             },
             heading: {
                 options: [
-                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' }
+                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+                    { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+                    { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+                    { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+                    { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' },
+                    { model: 'Formatted', view: 'pre', title: 'Formatted' }
                 ]
             }
         } )
@@ -27,4 +34,4 @@
             console.error( error );
         } );
 </script>
-@endsection
+@endpush
--- resources/views/pages/live-auction/create.blade.php
+++ resources/views/pages/live-auction/create.blade.php
@@ -11,7 +11,7 @@
             <div class="col-xl-6 col-lg-8 col-md-10">
                 <div class="form-wrapper wow fadeInUp" data-wow-duration="1.5s" data-wow-delay=".2s"
                     style="visibility: visible; animation-duration: 1.5s; animation-delay: 0.2s; animation-name: fadeInUp;">
-                    <form class="w-100" action="{{ route('user.register.handle') }}" method="POST">
+                    <form class="w-100" action="#" method="POST">
                         @csrf
                         <div class="row">
                             <div class="form-section">
@@ -33,6 +33,15 @@
                                 <x-input-field name="end_date" type="datetime-local" label="End Date" placeholder="Enter End Date" />
                             </div>
                             <div class="form-section">
+                                <h4>Category</h4>
+                            </div>
+                            <div class="col-md-6">
+                                <x-input-field name="category" type="text" label="Category" placeholder="Enter Category" />
+                            </div>
+                            <div class="col-md-6">
+                                <x-input-field name="sub_category" type="text" label="Sub Category" placeholder="Enter Sub Category" :required="false" />
+                            </div>
+                            <div class="form-section">
                                 <h4>Images</h4>
                             </div>
                             <div class="col-md-12">
@@ -43,25 +52,26 @@
                             </div>
                             {{-- add more images button --}}
                             <div class="col-md-12">
-                                <button class="images-btn">Add More Images</button>
+                                <button type="button" class="images-btn"><i class="bi bi-plus"></i> Add More Images</button>
                             </div>
                             <div class="form-section">
                                 <h4>Video</h4>
                             </div>
                             <div class="col-md-12">
                                 <x-input-field name="video" type="url" label="Video URL" placeholder="Enter Video URL" />
+                                <p class="text-muted">Please enter a valid video URL from YouTube or Vimeo, ex. https://www.youtube.com/watch?v=video_id </p>
                             </div>
                             <div class="form-section">
                                 <h4>Location</h4>
                             </div>
                             <div class="col-md-12">
-                                <x-input-field name="location" type="text" label="Location" placeholder="Enter Location" />
-                            </div>
-                            <div class="form-section">
-                                <h4>Category</h4>
+                                <x-input-field name="location" type="text" label="Location" placeholder="Enter Country" />
                             </div>
                             <div class="col-md-12">
-                                <x-input-field name="category" type="text" label="Category" placeholder="Enter Category" />
+                                <x-input-field name="state" type="text" label="State" placeholder="Enter State" />
+                            </div>
+                            <div class="col-md-12">
+                                <x-input-field name="city" type="text" label="City" placeholder="Enter City" />
                             </div>
                             <div class="form-section">
                                 <h4>Seller Information</h4>
@@ -100,3 +110,26 @@
 @include('layouts.metrics')
 
 @endsection
+@push('scripts')
+<script>
+    // Create a new input field for images
+    function createNewInputField() {
+        return `<div class="col-md-12">
+                    <x-input-field name="image[]" type="file" label="Upload Image" placeholder="Upload Image" />
+                </div>`;
+    }
+
+    // Add new input field for images
+    function addNewInputField() {
+        let newInputField = createNewInputField();
+        let imagesBtn = document.querySelector('.images-btn');
+        let newInputFieldElement = document.createElement('div');
+        newInputFieldElement.innerHTML = newInputField;
+        imagesBtn.parentNode.insertBefore(newInputFieldElement, imagesBtn);
+    }
+
+    // Add event listener to the button
+    document.querySelector('.images-btn').addEventListener('click', addNewInputField);
+
+</script>
+@endpush
