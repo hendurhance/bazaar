09482a08dad55c2eb8c88f7b0b84f6fcc7ef4217
Endurance, the Martian <afriviral@gmail.com>
2023-09-02 16:37:08 +0100

    refactor/ restructuring media to polymorphic relationship

--- app/Repositories/Ad/AdRepository.php
+++ app/Repositories/Ad/AdRepository.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Repositories\Repositories;
+namespace App\Repositories\Ad;
 
 use App\Abstracts\BaseCrudRepository;
 use App\Models\Ad;
 use App\Contracts\Repositories\AdRepositoryInterface;
 use App\Enums\AdStatus;
-use App\Models\Category;
 use App\Models\User;
 use App\Repositories\Category\CategoryRepository;
 use App\Repositories\Country\CountryRepository;
+use Illuminate\Support\Facades\DB;
 
 class AdRepository extends BaseCrudRepository implements AdRepositoryInterface
 {
@@ -27,9 +27,22 @@ class AdRepository extends BaseCrudRepository implements AdRepositoryInterface
      */
     public function create(?User $user, array $data): Ad
     {
-        // Handle media upload and creation
-        $media = $this->uploadMedia($data['images'], 'ads');
+        return DB::transaction(function () use ($user, $data) {
+            $ad = $this->store($user, $data);
 
+            return $ad;
+        });
+    }
+
+    /**
+     * Store an ad listing.
+     * 
+     * @param ?User $user|null
+     * @param array $data
+     * @return Ad
+     */
+    protected function store(?User $user, array $data): Ad
+    {
         $category = app(CategoryRepository::class)->findBySlug($data['category']);
         $subcategory = app(CategoryRepository::class)->findBySlug($data['subcategory']);
         $country = app(CountryRepository::class)->findByIso2Code($data['country']);
@@ -53,7 +66,6 @@ class AdRepository extends BaseCrudRepository implements AdRepositoryInterface
             'seller_email' => $data['seller_email'],
             'seller_mobile' => $data['seller_mobile'],
             'seller_address' => $data['seller_address'],
-
         ]);
     }
 }
--- database/migrations/2023_08_18_112706_create_media_table.php
+++ database/migrations/2023_08_18_112706_create_media_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('media', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignUuid('user_id')->nullable()->constrained('users')->onDelete('cascade');
-            $table->foreignUuid('mediaable_id')->index()->nullable();
-            $table->string('mediaable_type')->index()->nullable();
             $table->string('name')->nullable();
             $table->smallInteger('type')->nullable();
             $table->string('path')->nullable();
