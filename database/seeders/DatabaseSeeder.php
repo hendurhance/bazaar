b80e403fbba3ecd1a93a906a09a25c9e19c0fc7b
Endurance, the Martian <afriviral@gmail.com>
2023-09-11 18:13:41 +0100

    changes: minor fixes on live-auction show,index pages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             AtlasSeeder::class,
+            UserSeeder::class,
+            AdSeeder::class,
         ]);
     }
 }
